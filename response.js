d70cd843d4e7ee2ef066c42b0b2416a2ae74059c
= <=>
2023-04-10 08:09:28 +0530

    api is complete

--- response.js
+++ response.js
@@ -6,24 +6,55 @@ const getData = async () => {
     let month = date.toLocaleDateString('en-us',{month:"long",day:'2-digit'})
 
 
-    let bdata = {
-        name: "Aditya",
-        desc:"hello from client",
-        date:{
-            time:time,
-            date:month
-        }
-    }
-    const obj = {
+    
+    const post = {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
 
-        body: JSON.stringify(bdata)
+        body: JSON.stringify({
+            name: "testing post method",
+            desc:"posting",
+            date:{
+                time:time,
+                date:month
+            }
+            
+        })
 
     }
-    const data = await fetch("http://localhost:7575")
+
+    const put = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+
+        body: JSON.stringify({
+            name: "testing put method",
+            desc:"posting",
+            _id: '6433720c3c198f6e3e6255d0',
+            date:{
+                time:time,
+                date:month
+            }
+            
+        })
+
+    }
+    const deleteM = {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+
+        body: JSON.stringify({
+            _id: '6433720c3c198f6e3e6255d0'            
+        })
+
+    }
+    const data = await fetch("http://localhost:7575",deleteM)
     const res = await data.json()
     console.log(res)
 }
